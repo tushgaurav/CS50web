11842162bd68d5ddb60c5786a6ca954170fde25e
Tushar Gaurav <tushar.gaurav416@gmail.com>
2022-04-06 19:54:13 +0530

    Added wiki/TOPIC and DoesNot exist views

--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -4,7 +4,16 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entryview(request, title):
+    content = util.get_entry(title)
+    if content == None:
+        return render(request, "encyclopedia/notexist.html", {"title": title})
+
+    return render(
+        request,
+        "encyclopedia/view.html",
+        {"title": title, "content": content},
+    )
