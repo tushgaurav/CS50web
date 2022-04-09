297e29860dfa7fda0f81134012b95637eeea3cba
Tushar Gaurav <tushar.gaurav416@gmail.com>
2022-04-09 19:32:47 +0530

    added random wiki viewing functionality

--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from pydoc import resolve
+from django.shortcuts import redirect, render
 
 from . import util
 
+import re
+from random import randrange
+
 
 def index(request):
+    if request.method == "POST":
+        query = request.POST["q"].upper()
+        entry_list = util.list_entries()
+        if query in entry_list:
+            return redirect("/wiki/" + query)
+        for entry in entry_list:
+            result = re.search(query, entry)
+            if result:
+                return redirect("/wiki/" + entry)
+            return render(request, "encyclopedia/notexist.html", {"title": query})
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 
@@ -17,3 +31,9 @@ def entryview(request, title):
         "encyclopedia/view.html",
         {"title": title, "content": content},
     )
+
+
+def random(request):
+    entry_list = util.list_entries()
+    index = randrange(len(entry_list))
+    return redirect("/wiki/" + entry_list[index])
