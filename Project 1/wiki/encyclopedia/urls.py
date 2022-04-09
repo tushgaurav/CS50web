f990be682a5582f165811eb256d741d617ec28fc
Tushar Gaurav <tushar.gaurav416@gmail.com>
2022-04-09 19:34:32 +0530

    added random url

--- Project 1/wiki/encyclopedia/urls.py
+++ Project 1/wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.entryview, name="viewentry"),
+    path("random/", views.random, name="random"),
 ]
